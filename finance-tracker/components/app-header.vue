5396ce0cce68dd19552eaacdbfbb0a6eca1bc977
ydeguire <yoann.deguire@valtech.com>
2024-04-24 16:30:25 -0400

    first step with the finance app, adding supabase and light dark mode, working on index

---
+++ finance-tracker/components/app-header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="flex justify-between items-center mt-10">
+    <NuxtLink to="/" class="text-xl font-bold">Finance Tracker</NuxtLink>
+    <div class="flex items-center border p-3 space-x-2 border-gray-200 dark:border-gray-800">
+    <UAvatar
+        src="https://avatars.githubusercontent.com/u/739984?v=4"
+        alt="Avatar"
+    />
+    <ClientOnly>
+      <ColorModeSelector />
+    </ClientOnly>
+    </div>
+  </header>
+</template>
---
+++ finance-tracker/components/color-mode-selector.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex space-x-2 items-center group">
+        <div class="text-gray-500 text-xs invisible group-hover:visible">{{ currentMode }}</div>
+        <UToggle color="primary" v-model="isDarkMode" />
+    </div>
+  </template>
+  
+  <script setup>
+  const colorMode = useColorMode()
+  const isDarkMode = ref(colorMode.preference === 'dark')
+  const currentMode = computed(() => {
+    const mode = isDarkMode.value ? 'dark' : 'light'
+    colorMode.preference = mode;
+    return mode
+  })
+  </script>
